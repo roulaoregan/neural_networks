97f7e516b78abd7b227b93926fe1f20d12baf6d7
roulaoregan <roula.oregan@gmail.com>
2014-02-05 12:27:16 -0800

    Create driver.py
    adding driver for Btz machine

---
+++ driver.py
@@ -0,0 +1,16 @@
+'''
+Main driver for Boltzmann machine
+
+'''
+import logger
+import os
+import re
+import sys
+
+def main(argv=None):
+  pass
+  
+  
+if '__name__' == '__main__':
+  sys.exit(main())
+  
